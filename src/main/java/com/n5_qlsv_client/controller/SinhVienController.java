9ede07f60d174a6b8dbf701b7a9b0a77bd34f3c4
Development <70835318+Development707@users.noreply.github.com>
2021-10-15 06:49:58 +0700

    Home Page

--- src/main/java/com/n5_qlsv_client/controller/SinhVienController.java
+++ src/main/java/com/n5_qlsv_client/controller/SinhVienController.java
@@ -1,20 +1,16 @@
 package com.n5_qlsv_client.controller;
 
-import com.n5_qlsv_client.model.SinhVien;
-import com.n5_qlsv_client.service.SinhVienService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
 
 @Controller
 @RequestMapping("/")
 public class SinhVienController {
 
-    @Autowired
-    private SinhVienService sinhVienService;
+    @GetMapping
+    public String homePage(){
 
+        return "index";
+    }
 }
--- src/main/java/com/n5_qlsv_client/service/impl/SinhVienServiceImpl.java
+++ src/main/java/com/n5_qlsv_client/service/impl/SinhVienServiceImpl.java
@@ -1,33 +1,33 @@
-package com.n5_qlsv_client.service.impl;
-
-import com.n5_qlsv_client.model.SinhVien;
-import com.n5_qlsv_client.service.SinhVienService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.List;
-
-@Service
-public class SinhVienServiceImpl implements SinhVienService {
-
-    @Autowired
-    private RestTemplate restTemplate;
-
-    @Value("${app.url.sinhvien}")
-    private String url;
-
-    @Override
-    public List<SinhVien> getAllSinhViens() {
-        ResponseEntity<List<SinhVien>> responseEntity
-                = restTemplate.exchange(url, HttpMethod.GET, null,
-                new ParameterizedTypeReference<List<SinhVien>>() {
-                });
-        List<SinhVien> sinhVienList = responseEntity.getBody();
-        return sinhVienList;
-    }
-}
+//package com.n5_qlsv_client.service.impl;
+//
+//import com.n5_qlsv_client.model.SinhVien;
+//import com.n5_qlsv_client.service.SinhVienService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.core.ParameterizedTypeReference;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.stereotype.Service;
+//import org.springframework.web.client.RestTemplate;
+//
+//import java.util.List;
+//
+//@Service
+//public class SinhVienServiceImpl implements SinhVienService {
+//
+//    @Autowired
+//    private RestTemplate restTemplate;
+//
+//    @Value("${app.url.sinhvien}")
+//    private String url;
+//
+//    @Override
+//    public List<SinhVien> getAllSinhViens() {
+//        ResponseEntity<List<SinhVien>> responseEntity
+//                = restTemplate.exchange(url, HttpMethod.GET, null,
+//                new ParameterizedTypeReference<List<SinhVien>>() {
+//                });
+//        List<SinhVien> sinhVienList = responseEntity.getBody();
+//        return sinhVienList;
+//    }
+//}
