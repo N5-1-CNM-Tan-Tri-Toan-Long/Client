81dd3a253527ee60b74dd2f059309b56ef353594
Development <70835318+Development707@users.noreply.github.com>
2021-10-24 09:06:31 +0700

    SV - Lich Hoc
    Hoàn thành bio vs lịch học

---
+++ src/main/java/com/n5_qlsv_client/controller/HocPhanController.java
@@ -0,0 +1,9 @@
+package com.n5_qlsv_client.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/hoc-phan")
+public class HocPhanController {
+}
