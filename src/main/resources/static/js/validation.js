18244ce62486b2c51a4338d036a72593e231f004
ngoctri3110 <62044877+ngoctri3110@users.noreply.github.com>
2021-12-01 22:38:21 +0700

    Fix tính điểm

--- src/main/resources/static/js/validation.js
+++ src/main/resources/static/js/validation.js
@@ -19,11 +19,129 @@ function kiemtrastcth(){
 }
 
 function kiemtradtk1(){
-    var regexdtk1 = /^(0|1|2|3|4|5|6|7|8|9|10)([.][0-9])?$/;
+    var regexdtk1 = /^(\d+)([.]([\d]){0,2})?$/;
     var dtk1 = document.getElementById("txtdtk1").value;
+
     if (regexdtk1.test(dtk1)) {
-        document.getElementById('s3').innerHTML = "";
+        if (dtk1 > 10){
+            document.getElementById('btnTinhDiem').disabled = true;
+            document.getElementById('s3').innerText = "Phải nhập điểm từ 0.00 đến 10!";
+        } else{
+            document.getElementById('s3').innerText = "";
+            document.getElementById('btnTinhDiem').disabled = false;
+        }
     } else {
-        document.getElementById('s3').innerHTML = "Điểm không đúng định dạng !!!";
+        document.getElementById('btnTinhDiem').disabled = true;
+        document.getElementById('s3').innerText = "Phải nhập điểm từ 0.00 đến 10!";
+    }
+}
+function kiemtradtk2(){
+    var regexdtk1 = /^(\d+)([.]([\d]){0,2})?$/;
+    var dtk1 = document.getElementById("txtdtk2").value;
+
+    if (regexdtk1.test(dtk1)) {
+        if (dtk1 > 10){
+            document.getElementById('btnTinhDiem').disabled = true;
+            document.getElementById('s4').innerText = "Phải nhập điểm từ 0.00 đến 10!";
+        }
+        else{
+            document.getElementById('s4').innerText = "";
+            document.getElementById('btnTinhDiem').disabled = false;
+        }
+    } else {
+        document.getElementById('btnTinhDiem').disabled = true;
+        document.getElementById('s4').innerText = "Phải nhập điểm từ 0.00 đến 10!";
+    }
+}
+function kiemtradtk3(){
+    var regexdtk1 = /^(\d+)([.]([\d]){0,2})?$/;
+    var dtk1 = document.getElementById("txtdtk3").value;
+
+    if (regexdtk1.test(dtk1)) {
+        if (dtk1 > 10){
+            document.getElementById('btnTinhDiem').disabled = true;
+            document.getElementById('s5').innerText = "Phải nhập điểm từ 0.00 đến 10!";
+        }
+        else{
+            document.getElementById('s5').innerText = "";
+            document.getElementById('btnTinhDiem').disabled = false;
+        }
+
+    } else {
+        document.getElementById('btnTinhDiem').disabled = true;
+        document.getElementById('s5').innerText = "Phải nhập điểm từ 0.00 đến 10!";
+    }
+}
+function kiemtradgk(){
+    var regexdtk1 = /^(\d+)([.]([\d]){0,2})?$/;
+    var dtk1 = document.getElementById("txtdgk").value;
+
+    if (regexdtk1.test(dtk1)) {
+        if (dtk1 > 10){
+            document.getElementById('btnTinhDiem').disabled = true;
+            document.getElementById('s6').innerText = "Phải nhập điểm từ 0.00 đến 10!";
+        }
+        else{
+            document.getElementById('s6').innerText = "";
+            document.getElementById('btnTinhDiem').disabled = false;
+        }
+
+    } else {
+        document.getElementById('btnTinhDiem').disabled = true;
+        document.getElementById('s6').innerText = "Phải nhập điểm từ 0.00 đến 10!";
+    }
+}
+function kiemtradth1(){
+    var regexdtk1 = /^(\d+)([.]([\d]){0,2})?$/;
+    var dtk1 = document.getElementById("txtdth1").value;
+
+    if (regexdtk1.test(dtk1)) {
+        if (dtk1 > 10){
+            document.getElementById('btnTinhDiem').disabled = true;
+            document.getElementById('s7').innerText = "Phải nhập điểm từ 0.00 đến 10!";
+        }
+        else{
+            document.getElementById('s7').innerText = "";
+            document.getElementById('btnTinhDiem').disabled = false;
+        }
+    } else {
+        document.getElementById('btnTinhDiem').disabled = true;
+        document.getElementById('s7').innerText = "Phải nhập điểm từ 0.00 đến 10!";
+    }
+}
+function kiemtradth2(){
+    var regexdtk1 = /^(\d+)([.]([\d]){0,2})?$/;
+    var dtk1 = document.getElementById("txtdth2").value;
+
+    if (regexdtk1.test(dtk1)) {
+        if (dtk1 > 10){
+            document.getElementById('btnTinhDiem').disabled = true;
+            document.getElementById('s8').innerText = "Phải nhập điểm từ 0.00 đến 10!";
+        }
+        else{
+            document.getElementById('s8').innerText = "";
+            document.getElementById('btnTinhDiem').disabled = false;
+        }
+    } else {
+        document.getElementById('btnTinhDiem').disabled = true;
+        document.getElementById('s8').innerText = "Phải nhập điểm từ 0.00 đến 10!";
+    }
+}
+function kiemtradck(){
+    var regexdtk1 = /^(\d+)([.]([\d]){0,2})?$/;
+    var dtk1 = document.getElementById("txtdck").value;
+
+    if (regexdtk1.test(dtk1)) {
+        if (dtk1 > 10){
+            document.getElementById('btnTinhDiem').disabled = true;
+            document.getElementById('s9').innerText = "Phải nhập điểm từ 0.00 đến 10!";
+        }
+        else{
+            document.getElementById('s9').innerText = "";
+            document.getElementById('btnTinhDiem').disabled = false;
+        }
+    } else {
+        document.getElementById('btnTinhDiem').disabled = true;
+        document.getElementById('s9').innerText = "Phải nhập điểm từ 0.00 đến 10!";
     }
 }
